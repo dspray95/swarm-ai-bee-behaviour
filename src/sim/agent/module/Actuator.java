5dcebb363cd1fbeb6110990972f8b99c7b9b95ff
dspray95 <dspray95@gmail.com>
2018-03-22 06:03:19 +0000

    Actuator comments

--- src/sim/agent/module/Actuator.java
+++ src/sim/agent/module/Actuator.java
@@ -15,23 +15,28 @@ public class Actuator {
         this.options = parent.getOptions();
     }
 
+    /**
+     * Attempts to move the parent Agent to the target location
+     * @param target location to move to
+     * @return true if movement is possible, false otherwise
+     */
     public boolean Move(Coordinate target){
-
+        //Do not attempt to move if the parent is dead - simply return - probably a more efficient way to achieve this
         if(parent.getHP() <= 0){
             return true;
         }
-        else {
+        else { //If the target coordinate is already occupied - we can't move, so return false
             for (Agent agent : parent.getPerceptor().GetPerceivedBees()) {
                 if (agent.getLocation().X() == target.X() && agent.getLocation().Y() == target.Y() && parent.getClass() != Hornet.class) {
                     return false;
                 }
             }
         }
-
+        //return false if the target is outside the environment
         if(target.X() < 0 || target.Y() < 0 || target.X() > options.getEnvironmentSize() || target.Y() > options.getEnvironmentSize()){
             return false;
         }
-        else{
+        else{ //otherwise move the parent to the target
             parent.setLocation(target);
             return true;
         }
